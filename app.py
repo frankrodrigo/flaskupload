6e443d2160ed542a8476f251d1a5dd7242b86f5d
frankrodrigo <50206326+frankrodrigo@users.noreply.github.com>
2024-09-19 21:05:41 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,34 +1,31 @@
-from flask import Flask, request, redirect, url_for, render_template
+from flask import Flask, request, render_template
 from google.cloud import storage
-import os
 
 app = Flask(__name__)
 
 # Configure your bucket name here
 GCS_BUCKET = "flasktestbucket1"
 
-
 def upload_to_gcs(file, bucket_name, destination_blob_name):
     """Uploads a file to the bucket."""
-    storage_client = storage.Client()
+    # Create a client
+    storage_client = storage.Client()  # This can stay, it handles public access too
     bucket = storage_client.bucket(bucket_name)
     blob = bucket.blob(destination_blob_name)
 
     # Upload the file
     blob.upload_from_file(file)
 
-    # Make the file publicly accessible
+    # Make the file publicly accessible (optional)
     #blob.make_public()
 
     # Return the public URL
     public_url = f"https://storage.googleapis.com/{bucket_name}/{destination_blob_name}"
     return public_url
 
-
 @app.route('/')
 def index():
     return render_template('index.html')
-
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
@@ -48,6 +45,6 @@ def upload_file():
 
     return 'File upload failed'
 
-
 if __name__ == '__main__':
     app.run(debug=True)
+
